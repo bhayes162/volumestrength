b6ec3878a2d62dec653a3b850ac9b6edd7f1afcb
Bryan Hayes <66hayes@gmail.com>
2017-11-05 17:21:17 -0500

    update

---
+++ VolumeStrength/SignInViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SignInViewController.swift
+//  VolumeStrength
+//
+//  Created by Bryan Hayes on 11/5/17.
+//  Copyright © 2017 DiGiHayes. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseStorage
+import FirebaseDatabase
+
+
+class SignInViewController: UIViewController {
+
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    @IBAction func signinTapped(_ sender: Any) {
+    }
+    
+
+
+}
+
